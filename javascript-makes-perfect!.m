1874a273cb0219bfeaeede01c1476a6ce6df5a69
nixonkingko <31233523+nixonkingko@users.noreply.github.com>
2018-08-31 17:30:45 -1000

    Create javascript-makes-perfect!.m

---
+++ javascript-makes-perfect!.m
@@ -0,0 +1,18 @@
+---
+layout: essay
+type: essay
+title: JavaScript makes perfect!
+# All dates must be YYYY-MM-DD format!
+date: 2015-08-31
+labels:
+  - JavaScript
+  - Software Engineering
+  - WOD experiences
+  - Learning
+---
+
+I think JavaScript is a more efficient way of compiling the program. It is not like other programming languages (Java or C/C++) that can only be run through other virtual machines like JGRASP (for Java) or SSH Secure Shell Client (C/C++).  JavaScript itself can be interpreted directly through a browser (in the developer tools). In addition, JavaScript does not need to specify any type of variables unlike Java or C/C++ which need to declare a specific variable for example, like (integer, string). From my perspective, I think JavaScript is an excellent programming language for beginners due to its simplicity.
+
+In my opinion, JavaScript can be considered as a good programming language for software engineering because it is not only easy to debug and fix tracking errors but also have a good support such as HTML, CSS.  This is a Client Side Programming Language which processed and controlled by the client without any codes run on the side of the server. Then, it will make it easier for the client to control the web browser It is a high-level programming language and object-oriented.
+
+The practice in WODs is really important and useful as it teaches me to think on how to solve every problem and understand how exactly the code should be working in the other ways.  Basically, it is focusing on how important in adjusting the time acquired to be as short as possible in solving the problem by using the most efficient methods. Personally, I think this style of learning is a kind of stressful for me because solving the problem in a matter of time makes the adrenaline to increase higher especially if the problem cannot be justified or solved but I appreciate these WODs as it assists me to understand better in Javascript. Overall, I really want to explore more about JavaScript as this is a window of opportunities to learn the new programming languages and assist me to think logically in solving any designated problems.
